cb46e4340d6ebeda1cc9d126fb4a115e7e5a6f33
Gustavo Atanazio <gus.atanazio@gmail.com>
2025-09-20 13:20:26 -0300

    feat: abstração para CRUD

---
+++ src/main/java/com/totem_dasa/DAO/CRUD.java
@@ -0,0 +1,14 @@
+package com.totem_dasa.DAO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CRUD<E, I> {
+  // E - Entity, I - ID type
+
+  List<E> findAll(); // GET
+  Optional<E> findById(I id); // GET by ID
+  void create(E entity); // POST
+  void update(E entity); // PUT
+  void delete(I id); // DELETE
+}
