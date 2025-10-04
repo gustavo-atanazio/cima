fe235b0da8ed60bf7806663027a280f5f823c509
Gustavo Atanazio <gus.atanazio@gmail.com>
2025-10-04 19:24:43 -0300

    chore: deletando estrutura antiga

--- src/main/java/com/cima/services/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.cima.services;
-
-public class AuthService {
-  private final String TOKEN = "valid-token";
-
-  public void auth(String token) {
-    if (!TOKEN.equals(token)) throw new SecurityException("Token inválido.");
-  }
-}
