2f168ca563c73fd26b01ad31d76b067351ca82f1
Gustavo Atanazio <gus.atanazio@gmail.com>
2025-10-04 11:56:09 -0300

    feat: transformando em aplicação Spring

--- src/main/java/com/cima/App.java
+++ src/main/java/com/cima/App.java
@@ -1,32 +1,11 @@
 package com.cima;
 
-import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cima.services.AuthService;
-import com.cima.tests.StockMovementTest;
-import com.cima.tests.SupplyMovementTest;
-import com.cima.tests.TotemTest;
-
+@SpringBootApplication
 public class App {
-  public static void main(String[] args) {
-    AuthService authService = new AuthService();
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.print("Digite o token de autenticação: ");
-    String token = scanner.nextLine();
-
-    try {
-      authService.auth(token);
-      System.out.println("Autenticação bem-sucedida.");
-      
-      scanner.close();
-
-      TotemTest.testAll();
-      StockMovementTest.testAll();
-      SupplyMovementTest.testAll();
-    } catch (SecurityException e) {
-      System.out.println("Falha na autenticação: " + e.getMessage());
-      return;
-    }
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
